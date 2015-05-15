139b677037e163eba1f5793fb30c627335cc3cca
danilobecke <danilobecke@MacBook-Pro-de-Danilo-2.local>
2015-05-15 15:20:38 -0300

    animation 6

--- zonkedTransitionsSix.swift
+++ zonkedTransitionsSix.swift
@@ -8,6 +8,79 @@
 
 import UIKit
 
-class zonkedTransitionsSix: NSObject {
-   
+class zonkedTransitionsSix: NSObject, UIViewControllerAnimatedTransitioning, UIViewControllerTransitioningDelegate {
+    
+    private var presenting = true
+    
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        
+        
+        let container = transitionContext.containerView()
+        let fromView = transitionContext.viewForKey(UITransitionContextFromViewKey)!
+        let toView = transitionContext.viewForKey(UITransitionContextToViewKey)!
+        
+        
+        
+        
+        let offScreenRight = UIViewAnimationOptions.TransitionFlipFromRight
+        let offScreenLeft = UIViewAnimationOptions.TransitionFlipFromRight
+        
+        
+        
+        
+        container.backgroundColor = self.presenting ? toView.backgroundColor : fromView.backgroundColor
+        
+        
+        container.addSubview(fromView)
+//        container.addSubview(toView)
+
+        
+        
+        let duration = self.transitionDuration(transitionContext)
+        
+    
+        UIView.transitionFromView(container, toView: toView, duration: duration, options: presenting ? offScreenRight : offScreenLeft, completion: {
+            finished in transitionContext.completeTransition(true)
+        })
+        
+        
+//        UIView.animateWithDuration(duration, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: presenting ? offScreenRight : offScreenLeft, animations: {
+//            
+//            
+//            toView.alpha = 1
+//            fromView.alpha = 1
+//            
+//            
+//            }, completion: { finished in
+//                
+//                //  finished animating
+//                transitionContext.completeTransition(true)
+//                
+//        })
+        
+    }
+    
+    // return how many seconds the transiton animation will take
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
+        return 1.75
+    }
+    
+    // MARK: UIViewControllerTransitioningDelegate protocol methods
+    
+    // return the animataor when presenting a viewcontroller
+    // remmeber that an animator (or animation controller) is any object that aheres to the UIViewControllerAnimatedTransitioning protocol
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        
+        // these methods are the perfect place to set our `presenting` flag to either true or false - voila!
+        self.presenting = true
+        return self
+    }
+    
+    // return the animator used when dismissing from a viewcontroller
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        self.presenting = false
+        return self
+    }
+
 }
